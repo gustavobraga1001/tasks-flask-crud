fa2bd8e531ce3aa585159714c54d0c9deab4508c
gustavobraga1001 <braga.gustavo@outlook.com.br>
2025-01-09 10:19:23 -0300

    projeto finalizado

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 tasks = []
 task_id_control = 1
 
+""" Rotas da aplicação """
 @app.route('/tasks', methods=["POST"])
 def create_task():
     global task_id_control
@@ -34,6 +35,34 @@ def get_task(id):
             return jsonify(t.to_dict())
     return jsonify({"message": "Não foi possível encontrar atividade."}), 404
 
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar atividade."}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar atividade."}), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
